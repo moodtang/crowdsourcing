85e59b666b3c6f1a59d5ffe6fe0c52d5bf9feb5c
lenovo <1783742430@qq.com>
2018-11-02 08:58:28 +0800

    浏览器兼容

---
+++ src/components/view/home.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="head">
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "home"
+    }
+</script>
+
+<style scoped>
+  .head{
+    background: aqua;
+  }
+</style>
